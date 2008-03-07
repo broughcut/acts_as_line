1e2ac57e446aa390ce090cff0e3bd5ac98efca24
Jamie Brough <jb@tempestattack.com>
2008-03-06 19:33:33 -0800

    adds find methods

--- lib/acts_as_line.rb
+++ lib/acts_as_line.rb
@@ -9,6 +9,35 @@ module PutGIS
 
       def acts_as_line
         before_validation :draw_line
+
+        def touching(object, options={})
+          intersects(object,true,options)
+        end
+
+        def asunder(object, options={})
+          intersects(object,false,options)
+        end
+
+        def intersects(object, boolean, options={})
+          sql = []
+          sql << "SELECT * from #{self.table_name}"
+          sql << "WHERE (ST_Intersects(#{self.table_name}.geom,(SELECT geom FROM #{object.class.table_name} WHERE id = #{object.id})) = #{boolean})"
+          if options.size > 0
+            options.each_pair do |key,value|
+              if value.to_s.split('')[0] =~ /^>|^</
+                sql_value = value
+              else
+                sql_value = "= '#{value}'"
+              end
+              sql << "AND #{key} #{sql_value}"
+            end
+          end
+          if sql.size > 2
+            sql[2] = sql[2].gsub(/AND/,'AND (')
+            sql[-1] = sql[-1].gsub(/$/,')')
+          end
+          find_by_sql sql.join(' ')
+        end 
       end
     end
 
@@ -23,9 +52,6 @@ module PutGIS
       end
 
       module SingletonMethods
-        def find_intersecting
-          find(:all)
-        end
       end
 
     end
